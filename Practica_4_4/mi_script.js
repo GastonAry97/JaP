57b0edfc39915cb975d8625d7a4198d9fc13333b
GastonAry97 <110570338+GastonAry97@users.noreply.github.com>
2022-08-03 18:46:18 -0300

    Add files via upload

---
+++ Practica_4_4/mi_script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces de la página :
+    let html = document.getElementById('a').innerHTML;
+    alert(document.body.getElementsByTagName('info').length);
+
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", la dirección del penúltimo enlace de la página :
+
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces que apuntan a http://prueba/ :
+
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces del tercer párrafo :
+});
